1a44dae8785ce9638c4499c301129b6fdef77e60
darren-ct <darren0208.dc@gmail.com>
2022-09-07 12:26:13 +0700

    Commit

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize ("103.181.143.80","root", "NDiwno129*&e",{
-    host:"",
+const sequelize = new Sequelize ("dumbmerch","root", "NDiwno129*&e",{
+    host:"103.181.143.80",
     port : 3306,
     dialect: "mysql"
 });
